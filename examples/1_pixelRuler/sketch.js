12ff503917dd690f944dbde0d3851dbd25b571b9
alptugan <alptugan@gmail.com>
2023-02-01 19:54:54 +0300

    Update sketch.js
    pixel ruler enable/disable support

--- examples/1_pixelRuler/sketch.js
+++ examples/1_pixelRuler/sketch.js
@@ -2,26 +2,37 @@
 var utils = new p5.Utils();
 
 function setup() {
-  createCanvas(innerWidth, innerHeight);
+    createCanvas(innerWidth, innerHeight);
 
-  // Call the method "enableRuler()" 
-  // to display pixel ruler
-  utils.enableRuler();
+    // Set styling before the enabling the ruler
+    utils.setRulerStyle(
+        "rgba(200,200,200,1)", // Ruler Bg Color
+        "rgba(30,30,30,1)", // Ruler Text Color
+        40, // Ruler Size
+        "rgba(200,200,200,1)", // Info Text Color following the mouse cursor
+        "rgba(5,5,5,0.7)", // Info Text Background Color
+        "rgba(255,0,0,1)", // Ticker Color that projects the mouse cursor on top and left ruler bar
+        "10px monospace", // Overall font size and font family
+        false // Set true or false, to hide/show info card next to mouse cursor. By default it is true.
+    );
+    // Call the method "enableRuler()"
+    // to display pixel ruler
+    utils.enableRuler();
 }
 
 function draw() {
-  background(220);
+    background(220);
 
-  fill(255)
-  rect(200, 223, 200, 100);
+    fill(255);
+    rect(200, 223, 200, 100);
 }
 
 function keyPressed() {
-  if (key == 's') {
-    // re-draw ruler
-    utils.enableRuler();
-  } else if (key == 'h') {
-    // hide ruler
-    utils.disableRuler();
-  }
+    if (key == "s") {
+        // re-draw ruler
+        utils.enableRuler();
+    } else if (key == "h") {
+        // hide ruler
+        utils.disableRuler();
+    }
 }
